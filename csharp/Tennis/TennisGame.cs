c92404ff354f0627b2028aef5791a2a5bd858c8e
Kountzri <rick@kountz.us>
2021-06-21 16:13:04 -0400

    changed logic for advtage game remove some functions

--- csharp/Tennis/TennisGame.cs
+++ csharp/Tennis/TennisGame.cs
@@ -1,17 +1,22 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace Tennis
 {
     class TennisGame : ITennisGame
     {
-        private int m_score1 = 0;
-        private int m_score2 = 0;
+        //Renamed variables
+        private int player1Score = 0;
+        private int player2Score = 0;
+      
         private string player1Name;
         private string player2Name;
 
         public TennisGame(string player1Name, string player2Name)
         {
+            //Filling the Dictionary Terms
+            FillScoreDictionary();
             this.player1Name = player1Name;
             this.player2Name = player2Name;
         }
@@ -19,32 +24,71 @@ namespace Tennis
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
-                m_score1 += 1;
+                player1Score += 1;
             else
-                m_score2 += 1;
+                player2Score += 1;
         }
+        Dictionary<string, string> ScoreDictionary = new Dictionary<string, string>();
+
+        public void FillScoreDictionary()
+        {
+            //Basic Lingo for Scoring
+            ScoreDictionary.Add("0", "Love");
+            ScoreDictionary.Add("1", "Fifteen");
+            ScoreDictionary.Add("2", "Thirty");
+            ScoreDictionary.Add("3", "Forty");          
+        }
+
 
         public string GetScore()
         {
             string score = "";
-            //Simplifying Code so each part returns the most reduced code minimizing errors also added try catches
+            int leadValue, highestValue = 0;
+            //Getting absolute lead value doesn't matter which player
+            leadValue = Math.Abs(player1Score - player2Score);
+            //using the highest value from both players
+            highestValue = Math.Max(player1Score, player2Score);
+            //Removed extra methods changed rules for advantage to both players need at least 3
             try
             {
-                if (m_score1 == m_score2)
-                {
-                    //Game is Tied Return Tied Text
-                    score = TiedGame();
+                //Tied Game Less Than 3  
+                if (player1Score == player2Score && player1Score < 3 )
+                {                                   
+                    score = ScoreDictionary[player1Score.ToString()] + "-All";
                 }
-                else if (m_score1 >= 4 || m_score2 >= 4)
+                //Deuce for games greater 3 points or greater
+                else if (player1Score == player2Score && player1Score >= 3)
                 {
-                    //Game is Advantage to a player return Advantage Text
-                    score = GameAdvantage();
+                    score = "Deuce";
+                }
+                //Rules say BOTH players need at least 3 points not 4 for advantages
+                else if (player1Score >= 3 && player2Score >= 3)
+                {
+                   
+                    if(leadValue > 1)
+                    {
+                        score = "Win for ";
+                    }
+                    else { score = "Advantage "; }
+                    
+
+                    //Append player name with higher score
+                    if (player1Score > player2Score)                   
+                    { score += player1Name; }
+                    else { score += player2Name; }
+                }
+                else if(highestValue >=4 && leadValue >= 2)
+                {
+                    //Low Scoring Games for one player
+                    score = "Win for ";
+                    if(player1Score > player2Score) { score += player1Name; }
+                    else { score += player2Name; }
                 }
                 else
                 {
-                    //Score is neither tied nor are we at advantage
-                    score = MidGameScoreText();
+                    score = ScoreDictionary[player1Score.ToString()] + "-" + ScoreDictionary[player2Score.ToString()];
                 }
+               
             }
             catch (Exception ex)
             {
@@ -54,140 +98,8 @@ namespace Tennis
             }       
             return score;
         }
-        public string MidGameScoreText()
-        {
-            string midGameScoreText = "";
-            try
-            {
-              
-                var tempScore = 0;
-                for (var i = 1; i < 3; i++)
-                {
-                    if (i == 1)
-                    { tempScore = m_score1; }
-                    else
-                    { midGameScoreText += "-"; tempScore = m_score2; }
-                    switch (tempScore)
-                    {
-                        case 0:
-                            midGameScoreText += "Love";
-                            break;
-                        case 1:
-                            midGameScoreText += "Fifteen";
-                            break;
-                        case 2:
-                            midGameScoreText += "Thirty";
-                            break;
-                        case 3:
-                            midGameScoreText += "Forty";
-                            break;
-                    }
-                }
 
-            }
-            catch (Exception ex)
-            {
-                //Error Handling
-                Trace.WriteLine("An Error Occurred:" + ex.Message);
-               
-            }
-          
-            return midGameScoreText;
-        
-        }
-        public string TiedGame()
-        {
-            string gameTied = "";
-            try
-            {
-                switch (m_score1)
-                {
-                    case 0:
-                        gameTied = "Love-All";
-                        break;
-                    case 1:
-                        gameTied = "Fifteen-All";
-                        break;
-                    case 2:
-                        gameTied = "Thirty-All";
-                        break;
-                    default:
-                        gameTied = "Deuce";
-                        break;
-                }
-            }
-            catch (Exception ex)
-            {
-
-                Trace.WriteLine("An Error Occurred" + ex.Message);
-            }
-           
-            return gameTied;
-        }
-        public string ShutoutGame()
-        {
-            //Someone had a bad game
-
-            string shutoutText = "";
-
-            try
-            {
-                if (m_score1 > 2)
-                {
-                    shutoutText = "Win for player1";
-                }
-                else
-                { shutoutText = "Win for player2"; }
-            }
-            catch (Exception ex)
-            {
-
-                Trace.WriteLine("An error occurred" + ex.Message);
-              
-            }
-           
-            return shutoutText;
-        }
-        public string GameAdvantage()
-        {         
-            string advantageText = "";
-            try
-            {
-                //Didn't really need to change name just thought the readability improved over minusResult
-                var advantageDifference = m_score1 - m_score2;
-
-                if (advantageDifference > 2 || advantageDifference < -2)
-                {
-                    //Game will be ending
-                    advantageText = ShutoutGame();
-                }
-                else
-                {
-                    switch (advantageDifference)
-                    {
-                        case 1:
-                            advantageText = "Advantage player1";
-                            break;
-                        case -1:
-                            advantageText = "Advantage player2";
-                            break;
-                        case 2:
-                            advantageText = "Win for player1";
-                            break;
-                        default:
-                            advantageText = "Win for player2";
-                            break;
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                //Error Handling
-                Trace.WriteLine("An Error Occurred:" + ex.Message);
-            }
-          
-            return advantageText;
-        }
+       
     }
 }
 
